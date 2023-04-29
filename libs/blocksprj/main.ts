56368fe9aea7265232c12883ac9a47a46bb03dfc
Angaj Sharma <trancidence@gmail.com>
2023-04-29 17:42:34 +0530

    Added base from pxt-project

---
+++ libs/blocksprj/main.ts
@@ -0,0 +1,4 @@
+//% color=#002300 weight=300 icon="\uf1ec" 
+namespace timer {
+
+} 
---
+++ libs/core/ns.ts
@@ -0,0 +1,20 @@
+/**
+ * Basic functionalities.
+ */
+//% color=#002300 weight=300 icon="\uf1ec" 
+namespace timer {
+
+}
+
+//% color=#D4BC00 weight=95
+namespace hare {
+
+}
+
+/**
+ * Control flow
+ */
+//% color=#FF5722 weight=90
+namespace control {
+
+}
